c2678c496256b21c7846c13db9ba15735e38c8fa
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-03-30 17:31:57 -0300

    Cafetera.php comentado

--- Cafetera/Cafetera.php
+++ Cafetera/Cafetera.php
@@ -3,12 +3,14 @@ class Cafetera{
     private $capacidadMaxima;
     private $cantidadActual;
 
+    //FUNCION CONSTRUCTORA
     public function __construct($maxima, $actual)
     {
         $this->capacidadMaxima = $maxima;
         $this->cantidadActual = $actual;
     }
 
+    //FUNCIONES DE ACCESO
     public function getCapacidadMaxima(){
         return $this->capacidadMaxima;
     }
@@ -21,11 +23,19 @@ class Cafetera{
     public function setCantidadActual($x){
         $this->cantidadActual=$x;
     }
-
+    
+    /**
+     * Llena la cafetera (setea la cantidad actual igual a la capacidad maxima)
+     */
     public function llenarCafetera(){
         $this->setCantidadActual($this->getCapacidadMaxima());
     }
     
+    /**
+     * Dada una cantidad de cafe sirve una taza, es decir, resta de la cantidad actual la cantidad a servir
+     * solo en caso de que el cafe alcance. En caso de que el cafe no alcance, muestra un mensaje de error.
+     * @param int $cantidad
+     */
     public function servirTaza($cantidad){
         if($this->getCantidadActual()-$cantidad < 0){
             echo "No hay cafe suficiente.\n";
@@ -33,9 +43,18 @@ class Cafetera{
             $this->setCantidadActual($this->getCantidadActual()-$cantidad);
         }
     }
+
+    /**
+     * Setea la cantidad de cafe en 0.
+     */
     public function vaciarCafetera(){
         $this->setCantidadActual(0);
     }
+
+    /**
+     * Agrega la cantidad de cafe indicada por parametro. Si se sobrepasa la capacidad queda llena la cafetera.
+     * @param int $cantidad
+     */
     public function agregarCafe($cantidad){
         $this->setCantidadActual($this->getCantidadActual()+$cantidad);
         if($this->getCantidadActual()>$this->getCapacidadMaxima()){
@@ -43,6 +62,10 @@ class Cafetera{
             $this->setCantidadActual($this->getCapacidadMaxima());
         }
     }
+
+    /**
+     * Retorna un string con los atributos del objeto.
+     */
     public function __toString()
     {
         return "Capacidad maxima: " . $this->getCapacidadMaxima() . "\nCantidad Actual: " . $this->getCantidadActual() . "\n";
